48a27d4377ffbc896894221ca8ff85e691c27ac6
PiotrWardak <piotrekwardak@gmail.com>
2019-04-06 11:18:19 +0200

    Mapping classes: Category, Comment, Contact, Post - update

---
+++ src/main/java/pl/myblog/springblog/service/PostService.java
@@ -0,0 +1,15 @@
+package pl.myblog.springblog.service;
+
+import io.swagger.models.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.myblog.springblog.repository.PostRepository;
+
+@Service
+public class PostService {
+
+
+
+
+
+}
