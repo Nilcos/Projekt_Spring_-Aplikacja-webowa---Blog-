0e35928c3cc9c4a0669010796ff1bce9b3bc58bc
PiotrWardak <piotrekwardak@gmail.com>
2019-04-06 12:01:37 +0200

    Comment Service Added1

--- src/main/java/pl/myblog/springblog/service/CommentService.java
+++ src/main/java/pl/myblog/springblog/service/CommentService.java
@@ -1,4 +1,12 @@
 package pl.myblog.springblog.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.myblog.springblog.repository.CommentRepository;
+
+@Service
 public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
 }
