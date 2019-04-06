e7453ed3caa73e2c4f3d1e3860f3e113bbbd0cac
A.Maj <a.maj.maivs@gmail.com>
2019-04-06 12:33:17 +0200

    Edited PostController.

--- src/main/java/pl/myblog/springblog/controller/PostController.java
+++ src/main/java/pl/myblog/springblog/controller/PostController.java
@@ -34,12 +34,15 @@ public class PostController {
     @GetMapping("/")
     public String home(Model model, Authentication auth) {
         model.addAttribute("auth", auth);
+        model.addAttribute("postList", postService.getAllPosts());
         if (auth != null) {
             model.addAttribute("isAdmin", userService.isAdmin(auth));
             model.addAttribute("user", userService.getUserById(auth));
         } else {
             model.addAttribute("isAdmin", false);
         }
+
+
         return "index";
 
     }
